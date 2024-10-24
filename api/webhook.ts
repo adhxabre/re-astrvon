9ecac263e901f67b327ee90df1f70fa4ec863c64
adhxabre <abeldustin06@gmail.com>
2024-10-24 13:54:01 +0700

    Modify Vercel webhook config (#2)

--- api/webhook.ts
+++ api/webhook.ts
@@ -1,10 +1,10 @@
-import {VercelRequest, VercelResponse} from '@vercel/node';
+import { VercelRequest, VercelResponse } from '@vercel/node';
 import dotenv from 'dotenv';
-import {Client, GatewayIntentBits, Collection} from 'discord.js';
-import {Command} from '../src/types/Command';
-import {ExtendedClient} from '../src/types/ExtendedClient';
-import {handleEvents} from '../src/functions/handlers/handleEvents';
-import {handleCommands} from '../src/functions/handlers/handleCommands';
+import { Client, GatewayIntentBits, Collection } from 'discord.js';
+import { Command } from '../src/types/Command';
+import { ExtendedClient } from '../src/types/ExtendedClient';
+import handleEvents from '../src/functions/handlers/handleEvents';
+import handleCommands from '../src/functions/handlers/handleCommands';
 
 dotenv.config();
 
@@ -20,8 +20,8 @@ let isInitialized = false;
 
 async function initializeBot() {
     if (!isInitialized) {
-        await handleEvents(client);
-        await handleCommands(client);
+        handleEvents(client);
+        handleCommands(client);
         await client.login(process.env.TOKEN);
         console.log('Bot is logged in.');
         isInitialized = true;
@@ -31,9 +31,9 @@ async function initializeBot() {
 export default async (request: VercelRequest, response: VercelResponse) => {
     try {
         await initializeBot();
-        response.status(200).json({status: 'Bot is running!'});
+        response.status(200).json({ status: 'Bot is running!' });
     } catch (error) {
         console.error('Error initializing bot:', error);
-        response.status(500).json({status: 'Error initializing bot', error: error.message});
+        response.status(500).json({ status: 'Error initializing bot', error: error.message });
     }
 };
